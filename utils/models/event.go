75b83c7e3b34bce5b77f548e10e8390140ff4722
krishna zawar <zawarkrishna17@gmail.com>
2026-07-30 19:44:23 +0530

    chore: move models from editor to utils

--- utils/models/event.go
+++ utils/models/event.go
@@ -7,3 +7,19 @@ type Event interface {
 	// This is the identifier for the Event
 	GetEventName() string
 }
+
+// EventResponse is used to return back necessary info to the frontend for updates
+//
+// Not every response is sent to the frontend, only the necessary ones are sent
+type EventResponse struct {
+	Success    bool        `json:"success"` // whether the event was successful or not
+	Msg        string      `json:"msg"`     // the message describing the success or failure of the event
+	Data       interface{} `json:"data"`    // the updated data received from the event processing
+	ShouldEmit bool        `json:"-"`       // tells whether the data should be sent to the frontend or not
+}
+
+// EventHandler is used to handle the corresponding event that occured
+type EventHandler interface {
+	// Holds the actual logic on what happens when the event occurs
+	Handle(Event) *EventResponse
+}
