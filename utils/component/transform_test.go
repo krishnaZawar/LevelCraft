9dab3fb3966500facc2ed051675ca7722ec7611e
krishna zawar <zawarkrishna17@gmail.com>
2026-09-21 20:34:11 +0530

    fix(https://github.com/krishnaZawar/LevelCraft/issues/50): update persistence model and type check component unmarshalling

--- utils/component/transform_test.go
+++ utils/component/transform_test.go
@@ -1,8 +1,10 @@
 package component
 
 import (
+	"encoding/json"
 	"testing"
 
+	"github.com/krishnaZawar/LevelCraft/utils/component/base"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -36,4 +38,57 @@ func Test_UpdateTransform(t *testing.T) {
 
 	assert.Equal(t, w, actualW)
 	assert.Equal(t, h, actualH)
+}
+
+func Test_MarshalAndUnmarshalTransform(t *testing.T) {
+	x, y, w, h := 100, 120, 140, 160
+	expected := NewTransform(x, y, w, h)
+
+	found := newBaseTransform()
+
+	details := expected.GetComponentDetails()
+	err := found.BuildFromDetails(details.Data)
+	assert.Nil(t, err)
+
+	assert.Equal(t, base.ComponentName_Transform, details.Name)
+	assert.Equal(t, expected, found)
+}
+
+func Test_UnmarshalTransform(t *testing.T) {
+	t.Run("valid unmarshal", func(t *testing.T) {
+		data := json.RawMessage(
+			`{
+			"x": 100,
+			"y": 120,
+			"w": 140,
+			"h": 160
+			}`,
+		)
+		comp := newBaseTransform()
+		err := comp.BuildFromDetails(data)
+		assert.Nil(t, err)
+
+		assert.Equal(t, 100, comp.x)
+		assert.Equal(t, 120, comp.y)
+		assert.Equal(t, 140, comp.w)
+		assert.Equal(t, 160, comp.h)
+	})
+	t.Run("invalid unmarshal", func(t *testing.T) {
+		data := json.RawMessage(
+			`{
+			"x": 100,
+			"y": 120,
+			"w": 140
+			"h": 160
+			}`,
+		)
+		comp := newBaseTransform()
+		err := comp.BuildFromDetails(data)
+		assert.NotNil(t, err)
+
+		assert.Equal(t, defaultTransformValue, comp.x)
+		assert.Equal(t, defaultTransformValue, comp.y)
+		assert.Equal(t, defaultTransformValue, comp.w)
+		assert.Equal(t, defaultTransformValue, comp.h)
+	})
 }
